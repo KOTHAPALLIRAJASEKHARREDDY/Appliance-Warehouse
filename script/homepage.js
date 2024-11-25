649a6c3e4d212d70c29c4e54b4dc88b6c9bdace9
KOTHAPALLIRAJASEKHARREDDY <rajasekharreddy752@gmail.com>
2024-11-25 01:51:05 -0600

    Added placing order related changes

--- script/homepage.js
+++ script/homepage.js
@@ -43,5 +43,19 @@ setInterval(function() {
 
 function on_order(p_id)
 {
-    window.location.href = '/order?product_id='+p_id;
+     const url = '/order';
+     const params = new URLSearchParams({
+         product_id : p_id
+     });
+     fetch(`${url}?${params.toString()}`)
+         .then(response=>{
+             if(response.ok)
+             {
+                 window.location.href = response.url;
+             }
+             else {
+                 console.error('Issue with redirection.');
+             }
+         })
+            .catch(console.error);
 }
